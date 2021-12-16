dffe191204f3cfbf127898b8d737489bd066a7ad
Andrey Nasonov <andrey.nasonov@actonica.ru>
2021-12-16 16:35:55 +0700

    Add project files.

---
+++ ConsoleTest/PathTransformer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    internal class PathTransformer
+    {
+        private static readonly string _directorySeparatorString = Path.DirectorySeparatorChar.ToString();
+        private static readonly string _altDirectorySeparatorString = Path.AltDirectorySeparatorChar.ToString();
+        private static readonly string _specializedSeparator = "XXX XXX";
+        private const string _specializedSymbolSequence = "XXX";
+
+        /// <summary>
+        /// Gets searching version of local path.
+        /// Converts from C:\Windows\System32 to "XXXWindowsXXX XXXSystem32"
+        /// </summary>
+        /// <param name="localPath">Local path</param>
+        /// <returns>Searcing string representation</returns>
+        public static string GetSearchingPath(string localPath)
+        {
+            var result = PreparePath(localPath, _specializedSeparator);
+
+            if (localPath.StartsWith(_directorySeparatorString) || localPath.StartsWith(_altDirectorySeparatorString))
+                result = _specializedSymbolSequence + result;
+
+            if (localPath.EndsWith(_directorySeparatorString) || localPath.EndsWith(_altDirectorySeparatorString))
+                result += _specializedSymbolSequence;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets indexing version of local path.
+        /// Converts from C:\Windows\System32 to "XXXWindowsXXX XXXSystem32XXX"
+        /// </summary>
+        /// <param name="localPath">Local path</param>
+        /// <returns>Indexing string representation</returns>
+        public static string GetIndexingPath(string localPath)
+        {
+            var preparedPath = PreparePath(localPath, _specializedSeparator);
+            var result = _specializedSymbolSequence + preparedPath + _specializedSymbolSequence;
+            return result;
+        }
+
+        /// <summary>
+        /// Constructs well-formed string array from local path.
+        /// C:\Windows\System32 => "Windows System32".
+        /// </summary>
+        /// <returns>Well-formed array</returns>
+        private static string PreparePath(string localPath, string separator = " ")
+        {
+            var splittedArray = localPath.Split(@"\/".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var firstElement = splittedArray.FirstOrDefault();
+            var skipFirstElement = false;
+
+            if (firstElement != null && firstElement.Contains(Path.VolumeSeparatorChar))
+                skipFirstElement = true;
+
+            var wellFormedArray = skipFirstElement ? splittedArray.Skip(1) : splittedArray;
+            var joinedString = string.Join(separator, wellFormedArray);
+            return joinedString;
+        }
+
+        /// <summary>
+        /// Gets splitted version of folder local path.
+        /// Converts from C:\Windows\System32 to "Windows System32"
+        /// </summary>
+        /// <param name="localPath">Local path</param>
+        /// <returns>Splitted string version</returns>
+        public static string GetSplittedPath(string localPath)
+        {
+            var splittedPath = PreparePath(localPath);
+            return splittedPath;
+        }
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using TestLibrary;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        private const string SpecializedSymbolSequence = "XXX";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AssemblyTest();
+            //DirectoryInfoTest("Z:\\");
+            //TryParseChecks();
+            //OverflowChecks();
+            //LookupTest();
+            //TestFinallyThrow();
+            //TestConcurrentDictionary();
+            //ComposeUri(@"\\Andrew_k_j@hotmail.com\Inbox");
+            //ComposeUri(@"\\outlook data file\Inbox");
+            //MemoryUsingStrings();
+            //UriLeftPartBenchmark();
+            //CheckGuids();
+            //VirtualCOMObjects();
+            //NullOperations();
+            //TestPaths();
+
+            Console.ReadKey();
+        }
+
+        private static void AssemblyTest()
+        {
+            var knownImplementedTypes = Assembly.GetExecutingAssembly()
+                .GetReferencedAssemblies()
+                .SelectMany(a => Assembly.Load(a).GetTypes()
+                    .Where(t => t.IsAssignableTo(typeof(IMagic)) && !t.IsAbstract && !t.IsInterface));
+            var classNames = string.Join(", ", knownImplementedTypes.Select(t => t.Name));
+            Console.WriteLine(classNames);
+        }
+
+        private static void DirectoryInfoTest(string path)
+        {
+            var dirInfo = new DirectoryInfo(path);
+            var exists = dirInfo.Exists;
+        }
+
+        private static void TryParseChecks()
+        {
+            var nullableParsed = long.TryParse(null, out long nullableLong);
+            var emptyParsed = long.TryParse(string.Empty, out long emptyLong);
+        }
+
+        private static void OverflowChecks()
+        {
+            var rand = new Random();
+            int test = 0;
+
+            //obscured enough that the compiler doesn't "know" that the line will produce an overflow
+            //Does not run explicitly as checked, so no runtime OverflowException is thrown
+            test = rand.Next(Int32.MaxValue - 2, Int32.MaxValue) + 10;
+
+            //simple enough that the compiler "knows" that the line will produce an overflow
+            //Compilation error (line 16, col 10): The operation overflows at compile time in checked mode
+            //test = Int32.MaxValue + 1;
+
+            //Explicitly running as unchecked. Compiler allows line that is "known" to overflow.
+            unchecked
+            {
+                test = Int32.MaxValue + 1;
+            }
+
+            Console.WriteLine(test);
+
+            //Explicitly running as unchecked. Still no runtime OverflowException
+            unchecked
+            {
+                test = test - 10;
+            }
+
+            Console.WriteLine(test);
+
+            //Explicitly running as checked. System.OverflowException: Arithmetic operation resulted in an overflow.
+            checked
+            {
+                test = test + 10;
+            }
+
+            Console.WriteLine(test);
+        }
+
+        private static void LookupTest()
+        {
+            // Just types covering some different assemblies
+            Type[] sampleTypes = new[]
+            {
+                typeof(List<>),
+                typeof(string),
+                typeof(Enumerable)
+            };
+
+            // All the types in those assemblies
+            IEnumerable<Type> allTypes = sampleTypes.Select(t => t.Assembly).SelectMany(a => a.GetTypes());
+
+            // Grouped by namespace, but indexable
+            ILookup<string, Type> lookup = allTypes.ToLookup(t => t.Namespace);
+
+            foreach (Type type in lookup["System"])
+            {
+                Console.WriteLine("{0}: {1}", type.FullName, type.Assembly.GetName().Name);
+            }
+        }
+
+        private static void TestFinallyThrow()
+        {
+            try
+            {
+                try
+                {
+                    throw new Exception("exception thrown from try block");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Inner catch block handling {0}.", ex.Message);
+                    throw;
+                }
+                finally
+                {
+                    Console.WriteLine("Inner finally block");
+                    throw new Exception("exception thrown from finally block");
+                    Console.WriteLine("This line is never reached");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Outer catch block handling {0}.", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Outer finally block");
+            }
+        }
+
+        private static void TestConcurrentDictionary()
+        {
+            var dict = new ConcurrentDictionary<MyClass, MyClass>();
+            var item1 = new MyClass();
+            var resultItem1 = dict.GetOrAdd(item1, item1);
+
+            var item2 = new MyClass();
+            var resultItem2 = dict.GetOrAdd(item2, item2);
+
+            var eq = Equals(item1, resultItem2);
+            var refEq = ReferenceEquals(item1, resultItem2);
+
+            //Assert.Same(item1, resultItem2);
+        }
+
+        private static void ComposeUri(string path)
+        {
+            const string scheme = "outlook";
+
+            path = path.TrimStart('\\').Replace('\\', '/');
+            var testString = $"{scheme}:///{path}";
+
+            try
+            {
+                var uri = new Uri(testString, UriKind.RelativeOrAbsolute);
+
+                var properties = uri.GetType().GetProperties();
+                foreach (var uriProperty in properties)
+                {
+                    Console.WriteLine($"{uriProperty.Name} = {uriProperty.GetValue(uri)}");
+                }
+
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{testString}:\r\n{ex}");
+            }
+        }
+
+        private static void MemoryUsingStrings()
+        {
+            var strings = new List<string>();
+            for (int i = 0; i < 10000; i++)
+            {
+                strings.Add("00000000234275EDB5CDE74EB9AD0D47DCE16D400700DDB2F87163780843ADFEFD5D7910BCD50004F1E8063B0000DDB2F87163780843ADFEFD5D7910BCD50004F1E812B20000");
+            }
+            // +10000 string in array ~120 KB
+        }
+
+        private static void UriLeftPartBenchmark()
+        {
+            var uriArray = new List<Uri>();
+            for (int i = 0; i < 100000; i++)
+            {
+                uriArray.Add(new Uri("https://irradiant.sharepoint.com/sites/Dev/Shared%20Documents/Docs/emails?parentWeb=/sites/Dev&amp;folderType=Common"));
+            }
+
+            var concatenated = new List<string>();
+            var sw = Stopwatch.StartNew();
+            foreach (var uriInstance in uriArray)
+            {
+                concatenated.Add(uriInstance.Host + uriInstance.LocalPath);
+            }
+            sw.Stop();
+            Console.WriteLine($"Concatenated:\t{sw.Elapsed}");
+
+            sw.Reset();
+            var generated = new List<string>();
+            sw.Start();
+            foreach (var uriInstance in uriArray)
+            {
+                generated.Add(uriInstance.GetLeftPart(UriPartial.Path));
+            }
+            sw.Stop();
+            Console.WriteLine($"Generated:\t{sw.Elapsed}");
+        }
+
+        private static void CheckGuids()
+        {
+            // Check how fast Guid works
+            var list = new List<Foo>();
+            for (int i = 0; i < 2000; i++)
+            {
+                list.Add(new Foo(Guid.NewGuid()));
+            }
+        }
+
+        private static void VirtualCOMObjects()
+        {
+            // Obtain new first COM object
+            Foo someOtherFoo = new Foo("B");
+            // Save reference to COM object into variable
+            Foo foo = someOtherFoo;
+            // Rewrite new second COM object to the same first variable
+            someOtherFoo = new Foo("C");
+            // Then, someOtherFoo - 1st COM, foo - 2nd COM, NOT the same
+        }
+
+        private static void NullOperations()
+        {
+            // C# netcoreapp3.1
+            int? a = 10;
+            int? b = null;
+            a -= b;
+        }
+
+        private static void TestPaths()
+        {
+            string input;
+            while (!string.IsNullOrWhiteSpace(input = Console.ReadLine()))
+            {
+                Console.WriteLine("Indexing path: " + PathTransformer.GetIndexingPath(input));
+                Console.WriteLine("Searching path: " + PathTransformer.GetSearchingPath(input));
+                Console.WriteLine("Splitted path: " + PathTransformer.GetSplittedPath(input));
+                Console.WriteLine();
+            }
+        }
+
+        class Foo
+        {
+            public string SomeProperty { get; private set; }
+            public Guid Proper { get; }
+            public Foo(string bar) { SomeProperty = bar; }
+            public Foo(Guid guid) { Proper = guid; }
+        }
+
+        public static void CheckStaleIndices()
+        {
+            var indexedList = new List<string>()
+            {
+                @"C:\",
+                @"C:\Windows",
+                @"C:\Program"
+            };
+
+            var fetchedList = new List<string>()
+            {
+                @"C:\",
+                @"C:\Program",
+                @"C:\Info"
+            };
+
+            var newIndices = fetchedList.Except(indexedList);
+            var staleIndices = indexedList.Except(fetchedList);
+        }
+    }
+}
