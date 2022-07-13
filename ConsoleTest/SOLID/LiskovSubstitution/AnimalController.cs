1637ec370e5969b3dc459b6bae44398b054f951f
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-07-13 17:32:34 +0300

    Liskov substitution

---
+++ ConsoleTest/SOLID/LiskovSubstitution/AnimalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.SOLID.LiskovSubstitution
+{
+    internal class AnimalController
+    {
+        public static readonly List<Animal> Animals = new List<Animal>()
+        {
+            new Pigeon("pigeon")
+        };
+
+        public void AnimalLegCount(List<Animal> animals)
+        {
+            foreach (Animal animal in animals)
+            {
+                // Here we can provide all derived classes from Animal
+                // This method will not know what exactly class substituted
+                var legCount = animal.LegCount();
+            }
+        }
+    }
+}
---
+++ ConsoleTest/SOLID/LiskovSubstitution/Pigeon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.SOLID.LiskovSubstitution
+{
+    internal class Pigeon : Animal
+    {
+        public Pigeon(string name) : base(name)
+        {
+
+        }
+
+        public override int LegCount()
+        {
+            return 4;
+        }
+    }
+}
