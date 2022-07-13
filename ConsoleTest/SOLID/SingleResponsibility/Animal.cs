a62090f383c68b18d7483e8e7daa9667b1a57a30
Andrey Nasonov <37536541+Erapchu@users.noreply.github.com>
2022-07-13 16:49:03 +0300

    Added single responsibilty principle example

---
+++ ConsoleTest/SOLID/SingleResponsibility/Animal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest.SOLID.SingleResponsibility
+{
+    /// <summary>
+    /// Incorrect Animal class
+    /// </summary>
+    internal class WrongAnimal
+    {
+        public string Name { get; set; }
+
+        public WrongAnimal(string name)
+        {
+
+        }
+
+        public void SaveAnimal()
+        {
+            
+        }
+    }
+
+    /// <summary>
+    /// Correct Animal class according to Single Resbonsibility principle
+    /// </summary>
+    internal class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+
+        }
+    }
+
+    internal class AnimalDB
+    {
+        public Animal GetAnimal()
+        {
+            // Get animal from source
+            return null;
+        }
+
+        public void SaveAnimal(Animal animal)
+        {
+            // Save animal to source
+        }
+    }
+}
